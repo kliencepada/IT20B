33261199bf8a971d8876b273f9708535270f080a
kliencepada <jirehjamesc@gmail.com>
2025-11-18 08:25:21 +0800

    Added function computGrade

--- math.js
+++ math.js
@@ -1,23 +1,29 @@
-function add(a, b) {
-    return a + b;
-}
+function legalAge(age){
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    if (b === 0) {
-        return "Error: Division by zero!";
+    if( age >= 18){
+        console.log(" You are in a legal age");
+    } else {
+        console.log(" You are not in the legal age");
     }
-    return a / b;
 }
 
-console.log(add(2, 3));        // 8
-console.log(subtract(2, 3));  // 6
-console.log(multiply(2, 3));   // 42
-console.log(divide(2, 3));    // 4
+legalAge(69);
+
+function computeGrade(grade){
+
+    if (grade >= 94.5){\
+        console.log("1");
+    } else if (grade >= 91.5){
+        console.log("1.25");
+    } else if (grade >= 89.5){
+        console.log(1.25);
+    } else if (grade >= 86.5){
+        console.log ("1.75");
+    } else if (frade >= 84.5){
+        console.log("2");
+    } else {
+        console.log("Not within the transmutation");
+    }
+}
+
+computeGrade(87);
