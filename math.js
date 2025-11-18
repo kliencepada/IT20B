2a249668367ebdd7ce0a64ae43ef8fb416a93706
kliencepada <jirehjamesc@gmail.com>
2025-11-18 08:39:55 +0800

    Added function ticketPrice

--- math.js
+++ math.js
@@ -11,7 +11,7 @@ legalAge(69);
 
 function computeGrade(grade){
 
-    if (grade >= 94.5){\
+    if (grade >= 94.5){
         console.log("1");
     } else if (grade >= 91.5){
         console.log("1.25");
@@ -27,3 +27,37 @@ function computeGrade(grade){
 }
 
 computeGrade(87);
+
+function login(username,password){
+
+    if(username === "cliffy"){
+
+        if(password === 1234){
+             console.log("Welcome User");
+        } else {
+             console.log("Invalid login credentials");
+        }
+    } else{
+         console.log("Invalid User");
+    }
+}
+login("cliffy",1234)
+
+function ticketPrice(age){
+
+    if(age < 18){
+        if(age < 5){
+            console.log("Ticket: Free");
+        } else {
+            console.log("Ticket: Child Price")
+        }
+    } else {
+        if (age >= 60){
+            console.log("Ticket: Senior discount");
+        }else {
+            console.log("Ticket: Adult Price");
+        }
+    }
+}
+
+ticketPrice(70);
