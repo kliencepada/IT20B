8da8e53acc09954733eee9f1342adac2504ec17d
kliencepada <jirehjamesc@gmail.com>
2025-11-21 08:27:48 +0800

    Added array  while-loop

--- while-loop.js
+++ while-loop.js
@@ -25,12 +25,24 @@ while(i>=1){
 
 console.log(output);
 */
-
+/*
 output = " ";
 i = 1;
 
 while(i<=5){
     output += i + " ";
+    i++;
+}
+
+console.log(output);
+*/
+
+arr = [4,2,3];
+output = " ";
+i = 0;
+
+while(i<arr.length){
+    output += arr[i] + " ";
     i++;
 }
 
