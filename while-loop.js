d0f110bf73077d9971289de680ac6b6e8f1ceaac
kliencepada <jirehjamesc@gmail.com>
2025-11-21 08:23:42 +0800

    Added descending  while-loop

--- while-loop.js
+++ while-loop.js
@@ -6,12 +6,21 @@ while (i>=1){
     i--;
 }
 */
-
+/*
 i = 1;
 
 while(i<=5){
     console.log(i);
     i++;
 }
+*/
 
+output = " ";
+i = 5;
 
+while(i>=1){
+    output += i + " ";
+    i--;
+}
+
+console.log(output);
