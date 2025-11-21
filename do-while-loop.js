d5eedccceeb443754e737fc7a89c0df34e81ebb8
kliencepada <jirehjamesc@gmail.com>
2025-11-21 08:48:28 +0800

    Added function in do-while-loop

--- do-while-loop.js
+++ do-while-loop.js
@@ -6,10 +6,24 @@ do{
     i++;
 }while (i<=5);
 */
-
+/*
 i = 5;
 
 do{
     console.log(i);
     i--;
 }while (i>=1);
+*/
+
+arr1 = [4,2,3];
+function arrDoWhile(arr){
+output = "";
+
+i = 0;
+do{
+    output += arr[i] + " ";
+    i++;
+} while(i < arr.length);
+
+arrDoWhile(arr1);
+}
