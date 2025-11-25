9965e272d2aeaada77d42f907aee368a2d154b0e
kliencepada <jirehjamesc@gmail.com>
2025-11-25 08:22:08 +0800

    Added final delete step

--- array.js
+++ array.js
@@ -44,6 +44,9 @@ class Array {
         for(let i = index; i <this.length; i++){
             this.data[i] = this.data[i + 1];
         }
+
+        delete this.data[this.length - 1];
+        this.length--;
     }
 }
 
