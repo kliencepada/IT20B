d49c413e43cf8f52790ae93178e92b8a8ea55498
kliencepada <jirehjamesc@gmail.com>
2025-11-25 08:00:49 +0800

    Added initialization loop to load initialData into internal storage

--- array.js
+++ array.js
@@ -3,5 +3,10 @@ class Array {
     constructor(initialData = []){
         this.data = {};
         this.length = 0;
+
+        for(let i=0; i<initialData.length; i++){
+            this.data[i] = initialData[i];
+            this.length++;
+        }
     }
 }
